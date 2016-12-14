4f07dc2cb3aaa8a3d05aeb55b6333caa17a4ba3b
Dawid Pietkiewicz <dawidpietkiewicz@gmail.com>
2016-12-14 18:30:09 +0100

    tenerife-thumb.css

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+
+	$(function(){
+		     $(".nav-hid").hide();
+		     $(".hamburger-icon,.nav-hid").on("click",function() {
+		         $(".nav-hid").fadeToggle("slow");
+		     });
+		 });
+
+		
+
+    $(function(){
+        $(".hamburger-icon,.nav-hid").on("click",function(){
+            $(".hamburger-icon span:nth-child(1)").toggleClass("hamburger-x-1");
+            $(".hamburger-icon span:nth-child(2)").toggleClass("hamburger-x-2");
+            $(".hamburger-icon span:nth-child(3)").toggleClass("hamburger-x-3");
+        });
+    });
+
+		
+
+    $(function(){
+        $(window).on("load resize", function(){
+            $(".fill-screen").css("height", window.innerHeight);
+        });
+    });
+
+	
+
+    $(function () {
+        $(".down-button").on("click", function () {
+            $("body").animate({
+                scrollTop: ($(this).parents("div").next().offset().top)
+            }, 800);
+        });
+    });
+
+    if (window.matchMedia("(max-width: 992px)").matches) {
+        $(function () {
+            $('.others-1, .others-2, .others-3').css('filter', 'opacity(100%)');
+        });
+    };
